a806a62fbeec3979aea2cbc758db4d4870fb585e
Anıl Tanrıverdiler <186810775+aniltanriverdiler@users.noreply.github.com>
2026-03-24 22:30:28 +0300

    feat: add home screen with search bar

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,14 +1,36 @@
-import React from "react";
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  ScrollView,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
+
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+
+import SearchBar from "@/components/SearchBar";
 
 const Index = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text className="text-blue-500 text-center">Anil Tanriverdiler</Text>
-      </View>
-    </SafeAreaView>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute inset-0 w-full h-full" />
+
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+        {/* Search Bar Section */}
+        <View className="flex-1 mt-5">
+          <SearchBar placeholder="Search for a movie" />
+        </View>
+      </ScrollView>
+    </View>
   );
 };
 
